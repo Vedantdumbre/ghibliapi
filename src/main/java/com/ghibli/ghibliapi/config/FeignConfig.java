9d79879ed1b3774b4ff374ff404a6f26f902b6de
Vedant <vedantdumbre0706@gmail.com>
2025-06-27 21:05:52 +0530

    Added the Stability API

---
+++ src/main/java/com/ghibli/ghibliapi/config/FeignConfig.java
@@ -0,0 +1,9 @@
+package com.ghibli.ghibliapi.config;
+
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+}
