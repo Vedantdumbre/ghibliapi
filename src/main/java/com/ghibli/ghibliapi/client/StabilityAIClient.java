10f9cb7a098234eab60c58fa48d6c92a2edd004b
Vedant <vedantdumbre0706@gmail.com>
2025-06-29 11:54:35 +0530

    More specifications in StabilityAIClient.java

--- src/main/java/com/ghibli/ghibliapi/client/StabilityAIClient.java
+++ src/main/java/com/ghibli/ghibliapi/client/StabilityAIClient.java
@@ -1,13 +1,29 @@
 package com.ghibli.ghibliapi.client;
 
-
+import com.ghibli.ghibliapi.config.dto.TextToImageRequest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(
-        name = "stabilityAIClient",
-        url = "${stability.api.base-url}",
+        name = "stabilityAiClient",
+        url = "${stability.api.base-url}", // url from application.properties file
         configuration = com.ghibli.ghibliapi.config.FeignConfig.class
 )
-public class StabilityAIClient {
+public interface StabilityAIClient {
+
+    @PostMapping(
+            value = "/v1/generation/{engine_id}/text-to-image",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            headers = {"Accept = image/png"}
+    )
+    byte[] generateImageFromText(
+            @RequestHeader("Authorization") String authorizationHeader,
+            @PathVariable ("engine_id") String engineId,
+            @RequestBody TextToImageRequest requestBody
+    );
 
 }
