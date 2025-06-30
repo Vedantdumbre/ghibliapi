a2481570cdf2f07d6da90177ca0a1e9768022b1f
Vedant <vedantdumbre0706@gmail.com>
2025-06-30 21:20:50 +0530

    Backend completed

---
+++ src/main/java/com/ghibli/ghibliapi/controller/GenerationController.java
@@ -0,0 +1,46 @@
+package com.ghibli.ghibliapi.controller;
+import com.ghibli.ghibliapi.config.dto.TextGenerationRequestDTO;
+import com.ghibli.ghibliapi.service.GhibliArtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = {"http://localhost:3000" , "http://127.0.0.1:3000"})
+@RequiredArgsConstructor
+public class GenerationController {
+    private final GhibliArtService ghibliArtService;
+
+    @PostMapping(value = "/generate", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> generateGhibliArt(
+            @RequestParam("image") MultipartFile image,
+            @RequestParam("prompt") String prompt) {
+        try {
+            byte[] imageBytes = ghibliArtService.createGhibliArt(image, prompt);
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(imageBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @PostMapping(value = "/generate-from-text", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> generateGhibliArtFromText(@RequestBody TextGenerationRequestDTO requestDTO) {
+        try {
+            byte[] imageBytes = ghibliArtService.createGhibliArtFromText(
+                    requestDTO.getPrompt(),
+                    requestDTO.getStyle()
+            );
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_PNG).body(imageBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+
+}
--- src/main/java/com/ghibli/ghibliapi/service/GhibliArtService.java
+++ src/main/java/com/ghibli/ghibliapi/service/GhibliArtService.java
@@ -1,7 +1,9 @@
 package com.ghibli.ghibliapi.service;
 import com.ghibli.ghibliapi.client.StabilityAIClient;
-import lombok.Value;
+import com.ghibli.ghibliapi.config.dto.TextToImageRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class GhibliArtService {
@@ -13,4 +15,35 @@ public class GhibliArtService {
         this.stabilityAIClient = stabilityAIClient;
         this.apikey = apikey;
     }
+
+    public byte[] createGhibliArt(MultipartFile image, String prompt) {
+        String finalPrompt = prompt + ", in a beautiful, detailed anime style of studio ghibli.";
+        String engineId = "stable-diffusion-v1-6";
+        String stylePreset = "anime";
+
+        return stabilityAIClient.generateImageFromImage(
+                "Bearer " + apikey,
+                engineId,
+                image,
+                finalPrompt,
+                stylePreset
+        );
+    }
+
+    public byte[] createGhibliArtFromText(String prompt, String style) {
+        String finalPrompt = prompt + ", in a beautiful, detailed anime style of studio ghibli.";
+        String engineId = "stable-diffusion-v1-6";
+        String stylePreset = style.equals("general") ? "anime" : style.replace("_", "-");
+
+        TextToImageRequest requestPayload =new TextToImageRequest(finalPrompt, stylePreset);
+
+        return stabilityAIClient.generateImageFromText(
+                "Bearer " + apikey,
+                engineId,
+                requestPayload
+        );
+    }
+
+
+
 }
